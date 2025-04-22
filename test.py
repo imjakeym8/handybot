35ba8043b76f8c1f07b7b57ad6a7c6023a12cc9a
Jakey <jc.tacogue@gmail.com>
2025-04-22 08:05:28 +0800

    live testing my scripts..

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
-# Task: Get Active Day from an over 31-element list
-days = ['Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday'] # Start from January 1 which is Wednesday
-monthly_messages = [4, 9, 5, 9, 7, 5, 1, 1, 1, 6, 3, 4, 0, 7, 9, 3, 3, 1, 8, 9, 4, 10, 0, 9, 0, 7, 10, 10, 8, 1, 5]
+from telethon import TelegramClient
+from cryptography.fernet import Fernet
+import os
+from dotenv import load_dotenv
+import asyncio
 
-added_weekly_messages = [sum(monthly_messages[i::7]) for i in range(7)]
-active_day = max(added_weekly_messages)
-day_indexes = ", ".join(days[i] for i,v in enumerate(added_weekly_messages) if v == active_day)
 
-if day_indexes.count(",") == 6:
-    day_indexes = " "
-print(f"Answer: {day_indexes}")
+load_dotenv()
+dk = os.getenv("DKEY")
+f = Fernet(dk)
 
-# Output: returns days[i] with the most accumulated messages
+t1 = os.getenv('TELETHON_1')
+api_id = f.decrypt(t1).decode()
+t2 = os.getenv('TELETHON_2')
+api_hash = f.decrypt(t2).decode()
+
+client = TelegramClient("session_2", api_id, api_hash)
+
+async def main():
+    await client.start()
+    group = await client.get_entity("https://t.me/_")
+    print(f"Chat ID: {group.id}")
+
+asyncio.run(main())
