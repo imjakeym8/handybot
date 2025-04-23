44917e5d57d5145feee02180de24f16eb9bfb166
Jakey <jc.tacogue@gmail.com>
2025-04-23 22:27:20 +0800

    need to branch down code to functions on different scripts

--- test.py
+++ test.py
@@ -9,16 +9,22 @@ load_dotenv()
 dk = os.getenv("DKEY")
 f = Fernet(dk)
 
-t1 = os.getenv('TELETHON_1')
-api_id = f.decrypt(t1).decode()
-t2 = os.getenv('TELETHON_2')
-api_hash = f.decrypt(t2).decode()
+#t1 = os.getenv('TELETHON_1')
+#api_id = f.decrypt(t1).decode()
+#t2 = os.getenv('TELETHON_2')
+#api_hash = f.decrypt(t2).decode()
+#
+#client = TelegramClient("session_2", api_id, api_hash)
 
-client = TelegramClient("session_2", api_id, api_hash)
 
-async def main():
-    await client.start()
-    group = await client.get_entity("https://t.me/_")
-    print(f"Chat ID: {group.id}")
+#async def main():
+#    await client.start()
+#    group = await client.get_entity("https://t.me/")
+#    print(f"Chat ID: {group.id}")
 
-asyncio.run(main())
+with open(".env", "a") as file:
+    key = f.encrypt(alts.encode())
+    file.write(f"\nTELETHON_3 = {key.decode()}")
+
+another_key = f.decrypt(key).decode()
+print(another_key)
